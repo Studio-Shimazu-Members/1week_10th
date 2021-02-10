2045f25a5d1d4575a8c24a64acfad5ea50ac33d0
takafumi4488 <38578938+takafumi4488@users.noreply.github.com>
2021-02-11 00:36:13 +0900

    スタート画面_音源追加

---
+++ Assets/Scripts/Title/TitlePresenter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitlePresenter : MonoBehaviour
+{
+
+    public void Start()
+    {
+        SoundManager.instance.PlayBGM(SoundManager.BGM.Title);
+    }
+
+    public void OnNewGameButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
