ef89f99ab81f55b2e6d499a10fda6e49e1f4e380
takafumi4488 <38578938+takafumi4488@users.noreply.github.com>
2021-02-11 01:20:29 +0900

    Ranking_update

---
+++ Assets/Scripts/Main/Ranking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(110);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
