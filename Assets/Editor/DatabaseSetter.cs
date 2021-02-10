61c60a5b2abfe419451152200ddb7be2800af75f
嶋津恒彦 <simanezumi1989@gmail.com>
2021-02-11 00:36:09 +0900

    データベースにワードデータも反映

--- Assets/Editor/DatabaseSetter.cs
+++ Assets/Editor/DatabaseSetter.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -6,27 +6,37 @@ using UnityEngine;
 [CustomEditor(typeof(TileListEntity))]//拡張するクラスを指定
 public class DatabaseSetter : Editor
 {
+    // 表示用とスコア要は分けたほうがいいのかな？（学校/がっこう　のように）
+    List<List<string>> wordList = new List<List<string>>()
+    {
+        new List<string>(){"りんご", "あっぷる"},
+        new List<string>(){"ごりら"},
+        new List<string>(){"らっぱ"},
+        new List<string>(){"ぱんつ"},
+    };
 
-    /// <summary>
-    /// InspectorのGUIを更新
-    /// </summary>
     public override void OnInspectorGUI()
     {
         TileListEntity database = target as TileListEntity;
 
         if (GUILayout.Button("Button"))
         {
-            Debug.Log("ボタンを押した");
-            // exampleScript.Load();
-
+            // 画像データを取得
             Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath("Assets/Image/images_size_32.png").OfType<Sprite>().ToArray();
             database.tileList.Clear();
 
-            foreach (Sprite sprite in sprites)
+            for(int i=0; i<sprites.Length; i++)
             {
                 Panel panel = new Panel();
-                panel.sprite = sprite;
-                panel.words.Add("test");
+                panel.sprite = sprites[i];
+                if (i < wordList.Count)
+                {
+                    panel.words = wordList[i];
+                }
+                else
+                {
+                    panel.words.Add("ななし");
+                }
                 database.tileList.Add(panel);
             }
         }
