d42e5b09fa0fdc63b1244c58a1e434ce47ea71b8
takafumi4488 <38578938+takafumi4488@users.noreply.github.com>
2021-02-10 22:31:58 +0900

    進捗210210

---
+++ Assets/Scripts/TileListEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class TileListEntity : ScriptableObject
+{
+  public  List<Panel> tileList = new List<Panel>();
+
+
+
+}
+
+[Serializable]
+public class Panel
+{
+    public Sprite sprite;
+    public  List<string> words = new List<string>();
+
+
+}
