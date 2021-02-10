bbff3f3c179c9461d7a1a2d6ac982bea5d2d87f0
嶋津恒彦 <simanezumi1989@gmail.com>
2021-02-11 00:10:53 +0900

    データベースからランダムに選択し反映

---
+++ Assets/Scripts/Main/PanelManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    [SerializeField] TileListEntity tileListEntity = default;
+    // public const int PANEL_MAX = 20;
+    [SerializeField] Transform panelParent = default;
+    PanelCore[] panelCores;
+
+    private void Start()
+    {
+        // 表示用のオブジェクトを取得
+        panelCores = panelParent.GetComponentsInChildren<PanelCore>();
+        // データベースからランダムにデータを取得
+        Panel[] randomDataSet = GetRandomPanelDataSet(tileListEntity);
+        // 反映
+        SetPanels(randomDataSet);
+    }
+
+    void SetPanels(Panel[] panels)
+    {
+        for (int i = 0; i < panelCores.Length; i++)
+        {
+            panelCores[i].SetPanel(panels[i]);
+        }
+    }
+
+    // データベースからランダムにデータを持ってくる
+    Panel[] GetRandomPanelDataSet(TileListEntity database)
+    {
+        // データが壊れないようにコピーする
+        List<Panel> databaseCopy = new List<Panel>(database.tileList);
+
+        // 渡すデータを用意する
+        Panel[] panels = new Panel[panelCores.Length];
+        for (int i=0; i < panelCores.Length; i++)
+        {
+            // ランダムに選んで格納
+            int r = Random.Range(0, databaseCopy.Count);
+            panels[i] = databaseCopy[r];
+            // 選んだものは除外
+            databaseCopy.RemoveAt(r);
+            if (databaseCopy.Count == 0)
+            {
+                break;
+            }
+        }
+
+        return panels;
+    }
+
+
+}
