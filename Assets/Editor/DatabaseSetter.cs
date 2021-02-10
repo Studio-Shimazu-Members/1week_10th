22f3117bbde2a9d6ff3f1d89d54715d384f57bf6
嶋津恒彦 <simanezumi1989@gmail.com>
2021-02-11 00:24:43 +0900

    エディタ拡張を使ってデータベースへデータ反映

---
+++ Assets/Editor/DatabaseSetter.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TileListEntity))]//拡張するクラスを指定
+public class DatabaseSetter : Editor
+{
+
+    /// <summary>
+    /// InspectorのGUIを更新
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        TileListEntity database = target as TileListEntity;
+
+        if (GUILayout.Button("Button"))
+        {
+            Debug.Log("ボタンを押した");
+            // exampleScript.Load();
+
+            Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath("Assets/Image/images_size_32.png").OfType<Sprite>().ToArray();
+            database.tileList.Clear();
+
+            foreach (Sprite sprite in sprites)
+            {
+                Panel panel = new Panel();
+                panel.sprite = sprite;
+                panel.words.Add("test");
+                database.tileList.Add(panel);
+            }
+        }
+        base.OnInspectorGUI();
+
+    }
+}
